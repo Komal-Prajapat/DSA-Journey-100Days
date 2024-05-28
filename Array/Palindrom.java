239ae3553619e6b0712c69a5a09bd46326b49310
Komal-Prajapat <komalprajapat267@gmail.com>
2024-05-28 19:45:55 +0530

    ary first program

--- Array/Palindrom.java
+++ Array/Palindrom.java
@@ -1,16 +1,23 @@
-class Palindrom{
-    public static void main(String arg[]){
-        int ary[] = {1,2,3,2,1};
-        int newary[];
+class Palindrom {
+    public static void main(String arg[]) {
+        int ary[] = { 1, 2, 3, 2, 1 };
+        int flag = 0;
         int n = ary.length;
-    for (int i=0 ; i<= ary.length  ; i--) {
-                if(ary[i] != ary[n-i-1]){
-                    System.out.println("Not!");
-                }
-                else{
-                    System.out.println("YEs");
-                }
-       
-    }
+        for (int i = 0; i < n / 2; i++) {
+            if (ary[i] != ary[n - i - 1]) {
+                flag = 1;
+                break;
+            }
+
+        }
+
+        if (flag == 1) {
+
+            System.out.println("Not!");
+
+        } else {
+            System.out.println("Palindomic ary");
+
+        }
     }
 }
